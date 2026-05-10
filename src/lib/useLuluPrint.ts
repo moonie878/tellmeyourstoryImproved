d5c8fa5208beec604e14654b2966857171ee1a0f
moonie878 <mark.b.griffiths@hotmail.com>
2026-05-10 02:42:39 +0100

    trubook eddited

--- src/lib/useLuluPrint.ts
+++ src/lib/useLuluPrint.ts
@@ -96,7 +96,7 @@ export async function getLuluShippingCost(
         phone_number: shippingAddress.phone_number,
         email:        shippingAddress.email,
       },
-      shipping_level: 'MAIL',
+      shipping_level: 'GROUND',
     }),
   })
 
@@ -185,7 +185,7 @@ export function useLuluPrint() {
             phone_number: shippingAddress.phone_number,
             email:        shippingAddress.email,
           },
-          shipping_level: 'MAIL',
+          shipping_level: 'GROUND',
         }),
       })
 
