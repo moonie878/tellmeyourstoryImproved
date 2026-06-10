7107c90ee078924350b035f7ed567585d144da9f
moonie878 <mark.b.griffiths@hotmail.com>
2026-06-11 00:01:06 +0100

    tribute procing update

--- src/components/pricing/PricingTable.vue
+++ src/components/pricing/PricingTable.vue
@@ -189,7 +189,7 @@
                 Create a tribute video
               </router-link>
               <div class="text-right">
-                <p class="text-2xl font-bold text-[#1C1917]">£9.99</p>
+                <p class="text-2xl font-bold text-[#1C1917]">£19.99</p>
                 <p class="text-xs text-[#8C847E]">Preview free · Pay to download</p>
               </div>
             </div>
